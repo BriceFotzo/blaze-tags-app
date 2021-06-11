fe031e4d40f70ec3df3f912c16f0393ad4c37c9f
bricefotzo <brice.fotzo@hotmail.com>
2021-06-11 07:11:20 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the blaze of tags given theirs images.',
+    author='taggers(E. Lalou & B. Fotzo)',
+    license='MIT',
+)
